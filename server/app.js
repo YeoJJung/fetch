309cccd06203820ae31aeeb55c02761e94984e54
YeoJJung <duwjdals1005@naver.com>
2023-03-15 22:30:41 +0900

    postman

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let id = 2;
 const todoList = [
@@ -7,19 +8,26 @@ const todoList = [
         id: 1,
         text: "할일 1",
         done: false, // 안했다
-    }
+    },
 ];
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.send('Hello World');
 })
 
-app.get('api/todo', () => {
-    return todoList;
+app.get('/api/todo', (req, res) => {
+    res.json(todoList); //json형태로 todolist를 보내준다
 })
 
-app.post('api/todo', (req,res) => {
+app.post('/api/todo', (req, res) => {
     const {text, done} = req.body; //body에 text와 done을 받아서 보내겠다. 받기위해서는 req.body에 들어있을 것이다.
+    todoList.push({
+        id: id++, // 겹치지 않게 (고유하게) ++해주기
+        text,
+        done,
+    });
+    return res.send('success');
 })
 app.listen(3000, () => {
     console.log('server start!!');
+
 })
